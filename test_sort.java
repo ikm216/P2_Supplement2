d7119ccb3220b0a06eb32456f93747d172cb1099
ikm216 <ikmclassof22@gmail.com>
2024-11-29 18:56:02 -0500

    test:added test for the bubble sort

--- test_sort.java
+++ test_sort.java
@@ -1,3 +1,12 @@
+import org.junit.*;
+
 public class test_sort {
+    @Test
+    public void testBubble(){
+        int [] arr = {5, 3, 8, 6, 2};
+        int [] test_arr = {2, 3, 5, 6, 8};
+        sort.bubbleSort(arr);
+        Assert.assertArrayEquals(test_arr, arr);
+    }
     
 }
