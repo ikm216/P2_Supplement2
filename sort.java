4e0fa8c423bbc554260dc101ef2e922b9a26d1c9
ikm216 <ikmclassof22@gmail.com>
2024-11-29 19:15:00 -0500

    feat:added feature for the merge sort

--- sort.java
+++ sort.java
@@ -18,5 +18,38 @@ public class sort{
         }
     }
 
-    
+    public static int [] mergeSort(int [] arr){
+        if (arr.length <= 1) {
+        return arr;
+    }
+
+    int mid = arr.length / 2;
+    int[] left = Arrays.copyOfRange(arr, 0, mid);
+    int[] right = Arrays.copyOfRange(arr, mid, arr.length);
+
+    return merge(mergeSort(left), mergeSort(right));
+}
+
+private static int[] merge(int[] left, int[] right) {
+    int[] result = new int[left.length + right.length];
+    int i = 0, j = 0, k = 0;
+
+    while (i < left.length && j < right.length) {
+        if (left[i] <= right[j]) {
+            result[k++] = left[i++];
+        } else {
+            result[k++] = right[j++];
+        }
+    }
+
+    while (i < left.length) {
+        result[k++] = left[i++];
+    }
+
+    while (j < right.length) {
+        result[k++] = right[j++];
+    }
+
+    return result;
+    }
 }
