5036a9d612b5545598bda36126900fab8550f1a7
ikm216 <ikmclassof22@gmail.com>
2024-11-29 19:06:26 -0500

    test:added test for the merge sort

--- test_sort.java
+++ test_sort.java
@@ -8,5 +8,13 @@ public class test_sort {
         sort.bubbleSort(arr);
         Assert.assertArrayEquals(test_arr, arr);
     }
-    
+
+    @Test
+    public void testMerge(){
+        int [] arr = {10, 5, 2, 1, 8};
+        int [] test_arr = {1, 2, 5, 8, 10};
+        sort.mergeSort(arr);
+        Assert.assertArrayEquals(test_arr, arr);
+    }
+
 }
